d5dd6c2cf1fb90d55b37c0bc3b85a2dd754511b3
sripriya667 <sripriyaofficial2004@gmail.com>
2025-04-04 13:27:47 +0530

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'; // Ensure this import is present
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
+  
+  export default MyApp;
+  
